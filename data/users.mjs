e49740d7c0a741406dff3fa3919105109f53b163
lmfmay <176079985+lmfmay@users.noreply.github.com>
2024-10-11 12:02:04 -0700

    Drafted HATEOAS

--- data/users.mjs
+++ data/users.mjs
@@ -1,5 +1,5 @@
 export const users = [
-    { id: 1, username: "alice", email: "alice@example.com" },
-    { id: 2, username: "bob", email: "bob@example.com" },
-    { id: 3, username: "charlie", email: "charlie@example.com" }
+    { userId: 1, username: "alice", email: "alice@example.com" },
+    { userId: 2, username: "bob", email: "bob@example.com" },
+    { userId: 3, username: "charlie", email: "charlie@example.com" }
   ];
