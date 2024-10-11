2ee92c93dd068cdacb9c62c8eb31df93f90b736f
lmfmay <176079985+lmfmay@users.noreply.github.com>
2024-10-11 12:40:17 -0700

    Added get and post routes for api/user

--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -4,5 +4,36 @@ import { error } from "../utilities/error.mjs";
 
 let router = express.Router()
 
+//@route: api/users
+//@desc: GET all users / POST new user
+router.route('/')
+.get((req,res)=>{
+    const links = [
+        {
+          href: "/api/users/:userId",
+          rel: ":userId",
+          type: "GET"
+        },
+        {
+          href: "/api/users/:userId",
+          rel: ":userId",
+          type: "POST"
+        }
+      ];
+    res.json({users,links})
+})
+.post((req,res,next)=>{
+    if (req.body.username && req.body.email){ //if userfields complete
+        if (users.find((user) => {user.email == req.body.email})){ //if user exists
+            next(error(409, 'User email already exists'));
+        } else {
+            let user = {
+                id: users[users.length - 1].userId + 1,
+                username: req.body.username,
+                email: req.body.email,
+            };
+        }
+    } else {next(error(400,'Insufficient Data'))} //if userfields incomplete
+})
 
 export default router;
