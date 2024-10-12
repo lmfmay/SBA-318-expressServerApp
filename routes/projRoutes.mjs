a29ea95c897336200ace5650b274bed8f45b53c4
lmfmay <176079985+lmfmay@users.noreply.github.com>
2024-10-12 00:59:20 -0700

    Added projRoutes

--- routes/projRoutes.mjs
+++ routes/projRoutes.mjs
@@ -4,5 +4,62 @@ import { error } from "../utilities/error.mjs";
 
 let router = express.Router()
 
+//@route: api/projects
+//@desc: GET all projects
+router.route('/')
+.get((req,res)=>{
+    const links = [
+        {
+            href: "/api/projects/:id",
+            rel: "get project details",
+            type: "GET"
+        },
+        {
+            href: "/api/projects/:id",
+            rel: "change project details",
+            type: "PATCH"
+        },
+        {
+            href: "/api/projects/:id",
+            rel: "delete a project",
+            type: "DELETE"
+        }
+      ];
+    res.json({projects,links});
+})
+
+//@route: api/projects/:id
+//@desc: GET project details/ PATCH project details / DELETE a project
+router.route('/:id')
+.get ((req,res,next) => {
+    let proj = projects.find((proj)=>proj.id==req.params.id)
+    if (proj){
+        res.json(proj);
+    } else {next(error(404,'Project does not exist'))}
+})
+.patch((req,res,next) => {
+    let proj = projects.find((proj,index)=>{
+        if(proj.id==req.params.id){
+            for (const key in req.body) {
+                projects[index][key] = req.body[key]; //update project object at index
+            }
+            return true;
+        }
+    })
+    if (proj) {
+        res.json(proj);
+    } else {next(error(404,'Project does not exist'))}
+})
+.delete((req,res,next) => {
+    let proj = projects.find((proj,index)=>{
+        if(proj.id==req.params.id){
+            projects.splice(index,1);
+            return true;
+        }
+    })
+    if (proj) {
+        res.json(proj);
+    } else {next(error(404,'Project does not exist'))}
+});
 
 export default router;
--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -1,6 +1,7 @@
 import express from "express";
 import { users } from "../data/users.mjs";
 import { error } from "../utilities/error.mjs";
+import { projects } from "../data/projects.mjs";
 
 let router = express.Router()
 
@@ -13,11 +14,6 @@ router.route('/')
           href: "/api/users/:userId",
           rel: ":userId",
           type: "GET"
-        },
-        {
-          href: "/api/users/:userId",
-          rel: ":userId",
-          type: "POST"
         }
       ];
     res.json({users,links});
@@ -45,14 +41,24 @@ router.route('/:userId')
     let user = users.find((user)=>user.userId==req.params.userId)
     const links = [
         {
-        href: `/api/users/${req.params.userId}`,
-        rel: "update user details",
-        type: "PATCH"
+            href: `/api/users/${req.params.userId}`,
+            rel: "update user details",
+            type: "PATCH"
         },
         {
-        href: `/api/users/${req.params.userId}`,
-        rel: ":delete user",
-        type: "DELETE"
+            href: `/api/users/${req.params.userId}`,
+            rel: "delete user",
+            type: "DELETE"
+        },
+        {
+            href: `/api/users/${req.params.userId}/projects`,
+            rel: "user projects",
+            type: "GET"
+        },
+        {
+            href: `/api/users/${req.params.userId}/projects`,
+            rel: "create user projects",
+            type: "POST"
         }
     ]
     if (user){
@@ -84,5 +90,35 @@ router.route('/:userId')
     } else {next(error(404,'User does not exist'))}
 });
 
+//@route: api/users/:userId/projects
+//@desc: GET projects by one user / POST project for one user 
+router.route('/:userId/projects')
+.get ((req,res,next) => {
+    let userProjs = [];
+    if (!users.find((user)=>user.userId==req.params.userId)){
+        return next(error(404,'User does not exist'));
+    }
+    projects.forEach(proj => {
+        if (proj.userId == req.params.userId){
+            userProjs.push(proj);
+        }
+    });
+    if (userProjs.length > 0){
+        res.json(userProjs);
+    } else {next(error(404,'User does not have any projects'))}
+})
+.post((req,res,next)=>{
+    if (req.body.title && req.body.description && req.body.category){ //if projfields complete
+        let proj = {
+            id: projects[projects.length - 1].id + 1,
+            userId: req.params.userId,
+            title: req.body.title,
+            description: req.body.description,
+            category: req.body.category
+        };
+        projects.push(proj);
+        res.json(proj);
+    } else {next(error(400,'Insufficient Data'))} //if projfields incomplete
+    })
 
 export default router;
