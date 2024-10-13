a6333e272e964d6979f8dbcb4f5d5bfe15a3236b
lmfmay <176079985+lmfmay@users.noreply.github.com>
2024-10-12 22:28:55 -0700

    Set projcategories routes

--- routes/projCategoryRoutes.mjs
+++ routes/projCategoryRoutes.mjs
@@ -1,8 +1,12 @@
 import express from "express";
 import { categories } from "../data/projcategories.mjs";
+import { projects } from "../data/projects.mjs";
 import { error } from "../utilities/error.mjs";
 
 let router = express.Router()
 
+router.get('/',(req,res)=>{
+    res.json({categories});
+})
 
 export default router;
